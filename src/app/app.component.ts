0421f503f8d3d0fa1911a5e6bedf70991bf0ba35
otipz19 <tasitwork@gmail.com>
2024-08-10 12:53:40 +0300

    Added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { FiltersComponent } from './filters/filters.component';
 import { BasketComponent } from "./basket/basket.component";
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PizzaListComponent, FiltersComponent, BasketComponent, HeaderComponent],
+  imports: [PizzaListComponent, FiltersComponent, BasketComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
