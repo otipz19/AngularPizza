d1eace3ad64e583ed979b15805e9f9feb48e8dd9
otipz19 <tasitwork@gmail.com>
2024-08-10 12:46:41 +0300

    Added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { FiltersComponent } from './filters/filters.component';
 import { BasketComponent } from "./basket/basket.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PizzaListComponent, FiltersComponent, BasketComponent],
+  imports: [PizzaListComponent, FiltersComponent, BasketComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
