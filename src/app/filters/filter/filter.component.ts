367a8c177b668f96b18fccfc539598cdc0d68429
otipz19 <tasitwork@gmail.com>
2024-08-12 13:42:05 +0300

    Adjusted styles and improved filters component

--- src/app/filters/filter/filter.component.ts
+++ src/app/filters/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, ElementRef, input, output, viewChild } from '@angular/core';
 import { Filter } from './filter.model';
 
 @Component({
@@ -12,7 +12,13 @@ export class FilterComponent {
   filter = input.required<Filter>();
   choose = output<Filter>();
 
+  private labelElement = viewChild.required<ElementRef<HTMLLabelElement>>('label');
+
   onClick() {
     this.choose.emit(this.filter());
+  }
+
+  activate() {
+    this.labelElement().nativeElement.click();
   }
 }
--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { AfterViewInit, Component, computed, inject, viewChildren } from '@angular/core';
 import { FilterComponent } from "./filter/filter.component";
 import { Filter } from './filter/filter.model';
 import { filters as DEFINED_FILTERS } from './defined-filters';
@@ -12,13 +12,21 @@ import { RoundSpanComponent } from '../shared/round-span/round-span.component';
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
-export class FiltersComponent {
+export class FiltersComponent implements AfterViewInit {
   private readonly pizzaListService = inject(PizzaListService);
+
+  private filterComponents = viewChildren(FilterComponent);
   filters: Filter[] = DEFINED_FILTERS;
-
+  activeFilter? : Filter;
+  
   pizzaCount = computed(() => this.pizzaListService.filteredPizza().length);
-
+  
   onFilter(filter: Filter) {
     this.pizzaListService.setFilter(filter);
+    this.activeFilter = filter;
+  }
+
+  ngAfterViewInit(): void {
+    this.filterComponents()[0].activate();
   }
 }
